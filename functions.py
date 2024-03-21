0dc0f34affacb4fb909d74600f5f23b75d520ac5
TWKTemplar <carlegos101@gmail.com>
2024-03-21 07:31:38 -0500

    Added random creation function named SpawnRandom in functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,12 @@
 import random
 import numpy
 def SpawnRandom(board):
-
+    x = random.randrange(len(board))
+    y = random.randrange(len(board))
+    for i in range(5):
+        if(board[x][y] == 0):
+            board[x][y] = 2
+            return board
     return board
 
 def Collapse(board):
